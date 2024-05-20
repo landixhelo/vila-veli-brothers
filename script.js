a5a200f3e41ca664ea8ad0b9a5e0cf0d3af7da48
landi <landixhelo02@gmail.com>
2024-05-20 17:20:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplayHoverPause: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+});
+
+const elements = document.querySelectorAll(".fadeinleft");
+const elements1 = document.querySelectorAll(".fade-in");
+const elements2 = document.querySelectorAll(".fadeinright");
+const elements3 = document.querySelectorAll(".fadeindown");
+const elements4 = document.querySelectorAll(".fadeinup");
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.4,
+};
+const callbacks = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active-left");
+    }
+  });
+};
+const callbacks1 = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+};
+const callbacks2 = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active-right");
+    }
+  });
+};
+const callbacks3 = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active-down");
+    }
+  });
+};
+const callbacks4 = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active-up");
+    }
+  });
+};
+let observer = new IntersectionObserver(callbacks, options);
+let observer1 = new IntersectionObserver(callbacks1, options);
+let observer2 = new IntersectionObserver(callbacks2, options);
+let observer3 = new IntersectionObserver(callbacks3, options);
+let observer4 = new IntersectionObserver(callbacks4, options);
+elements.forEach((elements) => {
+  observer.observe(elements);
+});
+elements1.forEach((elements1) => {
+  observer1.observe(elements1);
+});
+elements2.forEach((elements2) => {
+  observer2.observe(elements2);
+});
+elements3.forEach((elements3) => {
+  observer3.observe(elements3);
+});
+elements4.forEach((elements4) => {
+  observer4.observe(elements4);
+});
